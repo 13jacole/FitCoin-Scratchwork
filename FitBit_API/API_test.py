d11bfec7361b46fc68c5cd81bc460737140dc049
13jacole <jakobcole13@gmail.com>
2022-01-05 21:10:06 -0500

    Successful Test

--- FitBit_API/API_test.py
+++ FitBit_API/API_test.py
@@ -1,7 +1,20 @@
-import requests
+import fitbit
+import gather_keys_oauth2 as Oauth2
+import pandas as pd 
+import datetime
 
 from FitBit_API_Keys import *
 
-x = requests.post('https://api.fitbit.com/oauth2/authorize')
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
 
-print(x.text)
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
+yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+today = str(datetime.datetime.now().strftime("%Y%m%d"))
+
+fit_statsHR = auth2_client.activities(date=today, user_id=None, data=None)
